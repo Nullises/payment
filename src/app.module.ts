d32cf26f3bdc5a4c730e00c932864f427cd55d61
nullises <texanico@gmail.com>
2024-07-31 19:02:21 -0400

    feature/health-check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PaymentsModule } from './payments/payments.module';
+import { HealthCheckModule } from './health-check/health-check.module';
 @Module({
-  imports: [PaymentsModule],
+  imports: [PaymentsModule, HealthCheckModule],
 })
 export class AppModule {}
